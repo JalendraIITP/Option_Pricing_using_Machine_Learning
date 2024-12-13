f9fcbf39a7a94c9f08f68a8acdd5b48ba0d7f217
Jalendra Gouda <101317808+JalendraIITP@users.noreply.github.com>
2024-12-13 17:39:54 +0530

    Update flaskServer.py

--- backend/flaskServer.py
+++ backend/flaskServer.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import numpy as np
 from joblib import load
@@ -71,4 +72,5 @@ def predict():
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
